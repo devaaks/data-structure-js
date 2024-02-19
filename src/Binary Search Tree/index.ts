8a79819cb9a4a47605a99d1aa150a5022eca4ec8
Dev AKS <devaks24k@gmail.com>
2024-02-19 22:46:46 +0530

    feat: add binary search tree skeleton

---
+++ src/Binary Search Tree/index.ts
@@ -0,0 +1,17 @@
+class BstNode {
+    data: number;
+    left: null;
+    right: null;
+    constructor(data: number) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    root: null;
+    constructor() {
+        this.root = null;
+    }
+}
