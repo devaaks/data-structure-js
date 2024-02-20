a54c7239b88550e22e2f923fc66dadf1dc57488f
Dev AKS <devaks24k@gmail.com>
2024-02-20 23:50:04 +0530

    feat: add insertNode in bst

--- src/Binary Search Tree/index.ts
+++ src/Binary Search Tree/index.ts
@@ -1,7 +1,7 @@
 class BstNode {
     data: number;
-    left: null;
-    right: null;
+    left: BstNode | null;
+    right: BstNode | null;
     constructor(data: number) {
         this.data = data;
         this.left = null;
@@ -10,8 +10,35 @@ class BstNode {
 }
 
 class Tree {
-    root: null;
+    root: BstNode | null;
     constructor() {
         this.root = null;
+    }
+
+    addNode(data: number) {
+        const newNode = new BstNode(data);
+        if (!this.root) {
+            this.root = newNode; 
+        } else {
+            this.insertNode(this.root, newNode);
+        }  
+    }
+
+    insertNode(node: BstNode, newNode: BstNode) {
+        if (newNode.data < node.data) {
+            if (!node.left) {
+                node.left = newNode;
+            } else {
+                // @ts-ignore
+                this.insertNode(node.right, newNode);
+            }
+        } else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                // @ts-ignore
+                this.insertNode(node.left, newNode);
+            }
+        }
     }
 }
